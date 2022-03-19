7f06a31917aca21355a2c371145d464db48274ac
luckychacha <358140284@qq.com>
2022-03-19 16:18:09 +0800

    init

--- src/hello/main.mo
+++ src/hello/main.mo
@@ -1,5 +1,86 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+
+actor Self {
+    public type Message = {
+        author: Text;
+        time: Time.Time;
+        message: Text;
+    };
+
+    public type Microblog = actor {
+        set_name: shared(Text) -> async ();
+        get_name: shared() -> async ?Text;
+        follow: shared(Principal) -> async ();
+        follows: shared query () -> async [Principal];
+        post: shared (Text) -> async ();
+        posts: shared query (Time.Time) -> async [Message];
+        timeline: shared (Time.Time) -> async [Message];
+    };
+
+    stable var author_name: ?Text = null;
+    public shared func set_name(name: Text) {
+        author_name := ?name;
+    };
+
+    public shared func get_name() : async ?Text {
+        author_name
+    };
+
+    stable var followed: List.List<Principal> = List.nil();
+
+    public shared func follow(id: Principal) : async () {
+        followed := List.push(id, followed);
+    };
+
+    public shared query func follows() : async [Principal] {
+        List.toArray(followed)
+    };
+
+    stable var messages: List.List<Message> = List.nil();
+
+    public shared (msg) func post(otp: Text, text: Text) : async () {
+        assert(otp == "yyds");
+        let message = {
+            author = Principal.toText(Principal.fromActor(Self));
+            message = text;
+            time = Time.now();
+        };
+        messages := List.push(message, messages);
+    };
+
+    // 仅返回指定时间之后的内容
+    public shared query func posts(since: Time.Time) : async [Message] {
+        var latest: List.List<Message> = List.nil();
+        var idx = 0;
+        while (idx < List.size(messages)) {
+            let tmp: ?Message = List.get<Message>(messages, idx);
+            switch (tmp) {
+                case null {};
+                case (?m) {
+                    if (m.time > since) {
+                        latest := List.push(m, latest);
+                    };
+                };
+            };
+            idx += 1;
+        };
+        List.toArray(latest)
+    };
+
+    // 仅返回指定时间之后的内容
+    public shared func timeline(since: Time.Time) : async [Message] {
+        var all: List.List<Message> = List.nil();
+        for (id in Iter.fromList(followed)) {
+            let canister: Microblog = actor(Principal.toText(id));
+            let msgs = await canister.posts(since);
+            for (msg in Iter.fromArray(msgs)) {
+                all := List.push(msg, all);
+            };
+        };
+
+        List.toArray(all)
     };
 };
--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -1,19 +1,193 @@
-import { hello } from "../../declarations/hello";
+import { hello, createActor, canisterId as my_id } from "../../declarations/hello";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const authors = {}
 
-  const name = document.getElementById("name").value.toString();
+function time_format(timestamp) {
+  var date = new Date(Number(timestamp));
+  let Y = date.getFullYear() + '-';
+  let M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+  let D = date.getDate() + ' ';
+  let h = date.getHours() + ':';
+  let m = date.getMinutes() + ':';
+  let s = date.getSeconds(); 
+  return Y+M+D+h+m+s;
+}
 
-  button.setAttribute("disabled", true);
+async function post() {
+  let post = document.getElementById("post")
+  let error = document.getElementById("error");
+  error.innerText = ""
+  post.disabled = true;
+  let content = document.getElementById("message")
+  let otp = document.getElementById("otp")
+  if (content.value.trim().length == 0) {
+    // console.log(content.value)
+    // console.log("empty content")
+  } else {
+    try {
+      await hello.post(otp.value, content.value);
+      content.value = ""
+      otp.value = ""
+    } catch (err) {
+      console.log(`err:${err}`);
+      error.innerText = "Post error!";
+    }
+  }
+  post.disabled = false;
+}
+var tmp_name = "";
+async function load_author_name() {
+  let author_name = await hello.get_name()
+  // console.log(`current author name is: ${author_name}`)
+  if (tmp_name != author_name[0]) {
+    authors[my_id] = author_name ? author_name[0] : my_id;
+    // console.log(authors)
+    tmp_name = authors[my_id]
+    document.getElementById("current_author_name").innerText = author_name[0]
+  }
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello.greet(name);
+async function load_my_follows() {
+  let follows = await hello.follows();
+  // console.log("follows");
+  // console.log(follows)
+  let follow_section = document.getElementById("follows");
+  follow_section.replaceChildren([])
+  for (var i = 0; i < follows.length; i++) {
+    let follow_item = document.createElement("p")
+    // if (follows[i].message.trim().length == 0) {
+    //   continue;
+    // }
+    // console.log(follows[i].toString());
+    // get tmp_canister_id and tmp_name
+    // save it into authors.
+    let tmp_canister_id = follows[i].toString();
+    let tmp_name = await createActor(tmp_canister_id).get_name()
+    follow_item.setAttribute("name", "follow");
+    follow_item.setAttribute("canister_id", tmp_canister_id);
+    follow_item.setAttribute("author_name", tmp_name);
+    follow_item.innerText = tmp_name && tmp_name[0] ? tmp_name[0] : tmp_canister_id
+    authors[tmp_canister_id] = tmp_name && tmp_name[0] ? tmp_name[0] : tmp_canister_id
+    follow_section.append(follow_item)
+  }
+  document.getElementsByName("follow").forEach(element => {
+    
+    element.onclick = async () => {
+      let author_name = element.getAttribute("author_name") 
+      let canister_id = element.getAttribute("canister_id")
+      // console.log(a + "::" + b);
 
-  button.removeAttribute("disabled");
+      document.getElementById("chosen_follow_posts").replaceChildren([]);
+      let tmp_posts = await createActor(canister_id).posts(100);
+      for (var i = 0; i < tmp_posts.length; i++) {
+        let tmp_p = document.createElement("p");
+        let time = time_format(tmp_posts[i].time.toString().substring(0, 13));
+        let tmp = `   *****   \r\nTime: ${time},\r\nAuthor Name: ${author_name},\r\nAuthor Canister ID: ${canister_id},\r\nMessage: ${tmp_posts[i].message}`;
+        tmp_p.innerText = tmp;
+        document.getElementById("chosen_follow_posts").append(tmp_p);
+      }
+      
+    }
+  })
+}
 
-  document.getElementById("greeting").innerText = greeting;
+async function load_my_timeline() {
+  let timeline = await hello.timeline(100);
+  // console.log("timeline");
+  // console.log(timeline)
+  let timeline_section = document.getElementById("timeline");
+  timeline_section.replaceChildren([])
+  for (var i = 0; i < timeline.length; i++) {
+    let timeline_item = document.createElement("p")
+    let time = time_format(timeline[i].time.toString().substring(0, 13));
+    let tmp_name = await createActor(timeline[i].author).get_name()
+    let tmp = `   *****   \r\nTime: ${time},\r\nAuthor Name: ${tmp_name},\r\nAuthor Canister ID: ${timeline[i].author},\r\nMessage: ${timeline[i].message}`;
+    timeline_item.innerText = tmp
+    timeline_section.append(timeline_item)
+  }
+}
 
-  return false;
-});
+async function submit_author_name() {
+  let author_name = document.getElementById("author_name")
+  // console.log(author_name.value);
+  let button = document.getElementById("submit_author_name")
+  button.disabled = true;
+  if (author_name.value.trim().length == 0) {
+    author_name.value = ""
+    // console.log("empty author name");
+    return;
+  }
+  try {
+    await hello.set_name(author_name.value);
+    // console.log("success:", author_name.value)
+    author_name.value = ""
+    load_author_name()
+  } catch (err) {
+    // console.log(`author_name err:${err}`);
+  }
+  author_name.value = ""
+  button.disabled = false;
+}
+
+var num_posts = -1;
+
+async function load_posts() {
+  let posts_section = document.getElementById("posts")
+  if (num_posts < 0) {
+    // init
+    num_posts = posts_section.children.length
+  }
+  let posts = await hello.posts(100)
+  // console.log(posts)
+  if (posts.length == num_posts) {
+    return
+  }
+  posts_section.replaceChildren([])
+  num_posts = posts.length;
+  for (var i = 0; i < posts.length; i++) {
+    let post_item = document.createElement("p")
+    if (posts[i].message.trim().length == 0) {
+      continue;
+    }
+    post_item.innerText = posts[i].message
+    posts_section.append(post_item)
+  }
+}
+
+function load_posts_by_id(canister_id) {
+  let posts_section = document.getElementById("other_posts")
+  posts_section.replaceChildren([])
+  let posts = createActor(canister_id).posts;
+  for (var i = 0; i < posts.length; i++) {
+    let post_item = document.createElement("p")
+    if (posts[i].message.trim().length == 0) {
+      continue;
+    }
+    post_item.innerText = posts[i].message
+    posts_section.append(post_item)
+  }
+}
+
+function load() {
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  let submit_author_name_button = document.getElementById("submit_author_name");
+  submit_author_name_button.onclick = submit_author_name;
+  load_posts();
+  load_author_name();
+  load_my_follows();
+  load_my_timeline();
+  document.getElementById("refresh_my_folows").onclick = load_my_follows;
+  document.getElementById("refresh_timeline").onclick = load_my_timeline;
+  // document.getElementById("refresh_author_name").onclick = load_author_name;
+  setInterval(
+    load_posts,
+    3000
+  )
+  // setInterval(
+  //   load_author_name,
+  //   3000
+  // )
+}
+window.load = load()
+
